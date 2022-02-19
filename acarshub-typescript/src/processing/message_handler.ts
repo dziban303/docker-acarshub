c3e3fabbbaba1a24288bcca07ef8fde8bf4b1caa
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2022-02-19 07:41:05 -0700

    Initial implementation of message handler class
    * Takes in an ADSB message and creates a new plane, or updates it.

---
+++ acarshub-typescript/src/processing/message_handler.ts
@@ -0,0 +1,144 @@
+import { adsb, adsb_plane, aircraft_icon, plane } from "src/interfaces";
+
+class MessageHandler {
+  planes: Array<plane> = [];
+  adsb_last_update_time: number = 0;
+
+  constructor() {}
+
+  acars_message() {}
+
+  adsb_message(adsb_positions: adsb) {
+    this.adsb_last_update_time = adsb_positions.now;
+
+    adsb_positions.aircraft.forEach((target) => {
+      const callsign = this.get_callsign(target);
+      const hex = this.get_hex(target);
+      const tail = this.get_tail(target);
+      const plane = this.match_plane_from_id(callsign, hex, tail);
+      if (plane) {
+        this.update_plane_position(target, plane);
+      } else {
+        this.planes.push({
+          callsign: callsign,
+          hex: hex,
+          tail: tail,
+          position: target,
+          identifiers: [], // TODO REMOVE ME
+          messages: [],
+          has_alerts: false,
+          num_alerts: 0,
+          last_updated: this.adsb_last_update_time,
+        });
+      }
+    });
+
+    // now loop through all of the planes and make sure their positions are still valid. Remove if not found
+    this.planes.forEach((plane, index) => {
+      if (
+        plane.position &&
+        plane.last_updated &&
+        plane.last_updated < this.adsb_last_update_time
+      ) {
+        this.planes[index].last_updated = undefined;
+        this.planes[index].position = undefined;
+      }
+    });
+  }
+
+  update_plane_position(
+    plane: adsb_plane | undefined = undefined,
+    index: number | undefined = undefined
+  ) {
+    if (!index || !plane) return;
+
+    this.planes[index].position = plane;
+    this.planes[index].last_updated = this.adsb_last_update_time;
+  }
+
+  match_plane_from_id(
+    callsign: string | null = null,
+    hex: string | null = null,
+    tail: string | null = null
+  ) {
+    Object.values(this.planes).forEach((plane, index) => {
+      if (callsign && plane.callsign === callsign) {
+        return index;
+      }
+      if (hex && plane.hex === hex) {
+        return index;
+      }
+      if (tail && plane.tail === tail) {
+        return index;
+      }
+    });
+
+    return undefined;
+  }
+
+  get_callsign(plane: adsb_plane): string {
+    return (
+      plane.flight && plane.flight.trim() !== ""
+        ? plane.flight.trim()
+        : plane.r && plane.r !== ""
+        ? plane.r
+        : plane.hex
+    )
+      .replace("~", "")
+      .replace(".", "")
+      .replace("-", "")
+      .toUpperCase();
+  }
+
+  get_sqwk(plane: adsb_plane): number {
+    return plane.squawk || 0;
+  }
+
+  get_alt(plane: adsb_plane): string | number {
+    return plane.alt_baro ? String(plane.alt_baro).toUpperCase() : 0;
+  }
+
+  get_speed(plane: adsb_plane): number {
+    return plane.gs ? plane.gs : 0;
+  }
+
+  get_tail(plane: adsb_plane): string {
+    return plane.r
+      ? plane.r.replace("-", "").replace(".", "").replace("~", "")
+      : <any>undefined;
+  }
+
+  get_hex(plane: adsb_plane): string {
+    return plane.hex
+      ? plane.hex
+          .replace("-", "")
+          .replace(".", "")
+          .replace("~", "")
+          .toUpperCase()
+      : <any>undefined;
+  }
+
+  get_baro_rate(plane: adsb_plane): number {
+    return plane.baro_rate ? plane.baro_rate : 0;
+  }
+
+  get_heading(plane: adsb_plane): number {
+    return plane.track || 0;
+  }
+
+  get_ac_type(plane: adsb_plane): string {
+    return plane.t || <any>undefined;
+  }
+
+  // get_icon(plane: string): aircraft_icon | null {
+  //   return this.adsb_planes[plane].icon || <any>undefined;
+  // }
+
+  get_lat(plane: adsb_plane): number {
+    return plane.lat || 0;
+  }
+
+  get_lon(plane: adsb_plane): number {
+    return plane.lon || 0;
+  }
+}
