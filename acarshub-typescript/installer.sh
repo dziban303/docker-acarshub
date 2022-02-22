9939b9928ff66f277c5876031ed32858d600909d
Fred Clausen <43556888+fredclausen@users.noreply.github.com>
2022-02-21 20:51:03 -0700

    Disable some features in the installer that aren't present yet

--- acarshub-typescript/installer.sh
+++ acarshub-typescript/installer.sh
@@ -10,12 +10,15 @@ rm -rf ../rootfs/webapp/templates/* || exit 1
 
 mkdir -p ../rootfs/webapp/static/images || exit 1
 mkdir -p ../rootfs/webapp/static/js || exit 1
-mkdir -p ../rootfs/webapp/static/sounds || exit 1
+# TODO: re-enable after sound is added back in
+# mkdir -p ../rootfs/webapp/static/sounds || exit 1
 mkdir -p ../rootfs/webapp/templates || exit 1
 
 # copy the assets to the correct place
 cp -r ./dist/static/images ../rootfs/webapp/static/ || exit 1
-cp -r ./dist/static/sounds ../rootfs/webapp/static/ || exit 1
+# TODO: re-enable after sound is added back in
+# cp -r ./dist/static/sounds ../rootfs/webapp/static/ || exit 1
 cp -r ./dist/static/js ../rootfs/webapp/static/ || exit 1
 cp -r ./dist/static/index.html ../rootfs/webapp/templates/ || exit 1
-cp -r ./dist/static/js/helppage.MD ../rootfs/webapp/templates/ || exit 1
+# TODO: re-enable after helppage is added back in
+# cp -r ./dist/static/js/helppage.MD ../rootfs/webapp/templates/ || exit 1
